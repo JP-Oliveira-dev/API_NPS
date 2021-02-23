73ffdd38313d86dc475a08f1ea34fb8795d6b072
João Pedro <joaopedrodeoliveira10@Hotmail.com>
2021-02-23 15:05:02 -0300

    Second Day

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,12 @@
+import 'reflect-metadata';
 import express from 'express';
+import "./database";
+import { router } from "./routes"
 
 const app = express();
 
-/**
- * GET => Buscar
- * POST => Salvar
- * PUT => Alterar
- * DELETE => deletar
- * PATCH => Alteração especifica
- */
-
-app.get('/', (req, res) => {
-    return res.json({ message: "Hello World - NLW 04" });
-});
-// 1 param => rota(recurso API)
-// 2 param => req, res
-
-app.post('/', (req, res) => {
-    // recebeu os dados para salvar
-    return res.json({ message: "Os dados foram salvos com sucesso!" })
-})
+app.use(express.json());
+app.use(router);
 
 
-app.listen(3333, () => console.log('Server is running!'));
+app.listen(3333, () => console.log('Server is Running!'))
